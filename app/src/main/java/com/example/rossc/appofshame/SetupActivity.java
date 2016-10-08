ecc6186518730c8c37186760af82bae6d94d3659
rossc <cwross204>
2016-10-08 00:57:40 -0400

    Fixed intent problems

--- app/src/main/java/com/example/rossc/appofshame/SetupActivity.java
+++ app/src/main/java/com/example/rossc/appofshame/SetupActivity.java
@@ -9,7 +9,6 @@ public class SetupActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
-
+        setContentView(R.layout.setup_activity);
     }
 }
