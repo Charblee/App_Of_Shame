941dc17b0f818dc802ee4b3997ad5c20e5429d06
rossc <cwross204>
2016-10-08 00:43:43 -0400

    Next step in the UI. THIS BUILD STILL BREAKS!!!

---
+++ app/src/main/java/com/example/rossc/appofshame/LaunchActivity.java
@@ -0,0 +1,27 @@
+package com.example.rossc.appofshame;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.launch_activity);
+
+        Button button = (Button)findViewById(R.id.git_drinking_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //TODO: Does this work?
+                Intent intent = new Intent(getApplicationContext(), SetupActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/rossc/appofshame/SetupActivity.java
@@ -0,0 +1,15 @@
+package com.example.rossc.appofshame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SetupActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
