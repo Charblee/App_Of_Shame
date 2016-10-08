5704116174866e2f9ca131d7a2c47755eb246e28
Charblee <[charlie.harb@icloud.com]git>
2016-10-08 14:02:32 -0400

    Added camera functionality to the camera button.

--- app/src/main/java/com/example/rossc/appofshame/MainActivity.java
+++ app/src/main/java/com/example/rossc/appofshame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rossc.appofshame;
 
 import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -26,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivity(intent);
+
             }
         });
 
@@ -38,3 +42,39 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+/*  @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), SetupActivity.class);
+                startActivity(intent);
+            }
+
+Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+startActivity(intent);
+
+
+
+
+            static final int REQUEST_IMAGE_CAPTURE = 1;
+
+private void dispatchTakePictureIntent() {
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+            */
+
