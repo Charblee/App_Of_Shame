fcfa0611616289b78c3f3dc7ee0c8ed2d5624a3a
Charblee <[charlie.harb@icloud.com]git>
2016-10-08 18:31:29 -0400

    Added a few pages and styles for the drunk test.

--- app/src/main/java/com/example/rossc/appofshame/TestPage.java
+++ app/src/main/java/com/example/rossc/appofshame/TestPage.java
@@ -1,7 +1,10 @@
 package com.example.rossc.appofshame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestPage extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class TestPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_page);
+
+        Button left_button = (Button) findViewById(R.id.left_button);
+        left_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
