0b10c68aea925869bbe88f367d709659ccf8eabe
Brojowski <alex.gajowski@gmail.com>
2016-10-08 07:56:25 -0400

    add 'not drunk' button and listener

--- app/src/main/java/com/example/rossc/appofshame/MainActivity.java
+++ app/src/main/java/com/example/rossc/appofshame/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rossc.appofshame;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        Button notDrunkBtn = (Button) findViewById(R.id.no_longer_drunk_btn);
+        notDrunkBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
