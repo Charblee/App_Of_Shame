a23b3bbc21c371dcfce7783a8a333a7c15ca9646
Charblee <[charlie.harb@icloud.com]git>
2016-10-08 17:27:59 -0400

    Added a few pages for the sobriety test.

--- app/src/main/java/com/example/rossc/appofshame/MainActivity.java
+++ app/src/main/java/com/example/rossc/appofshame/MainActivity.java
@@ -58,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Intent intent = new Intent(getApplicationContext(), SobrietyVerification.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/example/rossc/appofshame/TestPage.java
@@ -0,0 +1,13 @@
+package com.example.rossc.appofshame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_page);
+    }
+}
