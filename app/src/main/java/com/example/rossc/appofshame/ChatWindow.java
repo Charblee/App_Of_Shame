1ffce73a5a3f8d5fcbdf770d6a23737fcdefa419
rossc <cwross204>
2016-10-09 01:20:08 -0400

    Created a way to hook server into the chat for sending purposes

--- app/src/main/java/com/example/rossc/appofshame/ChatWindow.java
+++ app/src/main/java/com/example/rossc/appofshame/ChatWindow.java
@@ -1,14 +1,13 @@
 package com.example.rossc.appofshame;
 
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 
 public class ChatWindow extends AppCompatActivity implements ChatService.IMessageListener {
 
@@ -18,8 +17,16 @@ public class ChatWindow extends AppCompatActivity implements ChatService.IMessag
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_window);
         ChatService service = ChatService.Instance(ChatWindow.this);
-
         service.registerChatView(this);
+
+        Button sendButton = (Button)findViewById(R.id.chat_send_button);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText)findViewById(R.id.chat_send_message);
+                // TODO: SendToServer(editText.getText());
+            }
+        });
     }
 
     @Override
