e861fd23dfe95dfc16e53116ddce556b3023a655
Charblee <[charlie.harb@icloud.com]git>
2016-10-08 22:49:08 -0400

    I added an alertdialog section.... that doesn't work properly.

--- app/src/main/java/com/example/rossc/appofshame/TestPage.java
+++ app/src/main/java/com/example/rossc/appofshame/TestPage.java
@@ -1,6 +1,8 @@
 package com.example.rossc.appofshame;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +10,45 @@ import android.widget.Button;
 
 public class TestPage extends AppCompatActivity {
 
+    /* public void onClick(View arg0) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        alertDialogBuilder.setTitle("Nope! You're still drunk!");
+
+        alertDialogBuilder
+            .setMessage("Exit")
+            .setCancelable(false)
+            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    TestPage.this.finish();
+                }
+            });
+    } */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_page);
 
-        Button left_button = (Button) findViewById(R.id.left_button);
+        final Button left_button = (Button) findViewById(R.id.left_button);
         left_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(TestPage.this);
 
+                alertDialogBuilder.setTitle("Nope! You're still drunk!");
+
+                alertDialogBuilder
+                        .setMessage("Exit")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                TestPage.this.finish();
+                            }
+                        });
             }
         });
-
         Button right_button = (Button) findViewById(R.id.right_button);
         right_button.setOnClickListener(new View.OnClickListener() {
             @Override
